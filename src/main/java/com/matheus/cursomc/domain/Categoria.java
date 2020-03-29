131d21701f204dfee9471c86f349112e86682fb1
Matheus Pereira <matheus191999@hotmail.com>
2020-03-29 04:16:37 -0300

    Adicionado novas dependencias, adicionado os properties do database h2 e alterado o model de categoria

--- src/main/java/com/matheus/cursomc/domain/Categoria.java
+++ src/main/java/com/matheus/cursomc/domain/Categoria.java
@@ -2,6 +2,10 @@ package com.matheus.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,9 +14,12 @@ import lombok.Data;
  * @author Matheus
  */
 @Data
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
