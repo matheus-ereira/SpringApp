d2812b1eebd57df9b74b45f42094ac297c8fd982
Matheus Pereira <matheus191999@hotmail.com>
2020-04-19 16:35:23 -0300

    Criado Lazy loading para o endpoint de categorias

--- src/main/java/com/matheus/cursomc/services/CategoriaService.java
+++ src/main/java/com/matheus/cursomc/services/CategoriaService.java
@@ -8,6 +8,9 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,26 +19,26 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CategoriaService {
-    
+
     @Autowired
     private CategoriaRepository repository;
-    
+
     public Categoria find(Integer id) {
         Optional<Categoria> obj = repository.findById(id);
         return obj.orElseThrow(() -> new NotFoundException(
                 "Objeto de id " + id + " não encontrado!!"));
     }
-    
+
     public Categoria insert(Categoria categoria) {
         categoria.setId(null);
         return repository.save(categoria);
     }
-    
+
     public Categoria update(Categoria categoria) {
         find(categoria.getId());
         return repository.save(categoria);
     }
-    
+
     public void delete(Integer id) {
         find(id);
         try {
@@ -44,8 +47,13 @@ public class CategoriaService {
             throw new DataIntegrityException("Não é possivel excluir categorias que possuem produtos!!");
         }
     }
-    
+
     public List<Categoria> findAll() {
         return repository.findAll();
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        return repository.findAll(pageRequest);
     }
 }
